24fd615795f0c618f897643a43619087e5ce2929
Christopher Huynh <email": null>
2017-03-10 15:08:50 -0800

    add user login logout and register

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ActiveRecord::Base
   has_many :games
+
+include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def authenticate(pw_input)
+    self.password == pw_input
+end
+
 end
